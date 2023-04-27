c4b8efc163eb3790ccb2d8b1624a405116d89efa
anna-banana-wilson <akw02@icloud.com>
2023-04-26 21:22:51 -0400

    fin scan in mode 3

--- mode3.py
+++ mode3.py
@@ -1,5 +1,14 @@
 from scapy.all import * 
-from pythonping import ping 
+#from pythonping import ping 
 
 def mode3(): 
     print("you chose mode 3") 
+
+    # ip1 = IP(src="50.106.175.86", dst ="131.229.72.13")
+    # sy1 = TCP(dport=22, flags="F", seq=12345)
+    # packet = ip1/sy1
+    packet =  IP(src="50.106.175.86", dst ="131.229.72.13")/TCP(dport=22, flags="F", seq=12345)
+    p = sr1(packet, timeout=2)
+    p.show()
+
+mode3()
