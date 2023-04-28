12e48610e186129d200db862c8f9e68754aa965c
anna-banana-wilson <akw02@icloud.com>
2023-04-28 18:16:04 -0400

    trying things out to get the ports on the host

--- mode3.py
+++ mode3.py
@@ -1,5 +1,5 @@
 from scapy.all import * 
-#from pythonping import ping 
+import socket
 
 def mode3(): 
     print("you chose mode 3") 
@@ -9,6 +9,9 @@ def mode3():
     # packet = ip1/sy1
     packet =  IP(src="50.106.175.86", dst ="131.229.72.13")/TCP(dport=22, flags="F", seq=12345)
     p = sr1(packet, timeout=2)
-    p.show()
+    packet.summary(lfilter = lambda s,r: r.sprintf("%TCP.sport% \t %TCP.flags%"))
+    #serviceName = socket.getservbyport(1, 'tcp') # this returns just the service name 
+    #print(serviceName)
+    # print("Name of the service running at port number %d : %s"%(portNumber, serviceName));
 
 mode3()
