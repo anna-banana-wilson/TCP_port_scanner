5d4e6daf408b78c1940b399e6fb9a99d8ecc402b
anna-banana-wilson <akw02@icloud.com>
2023-04-28 20:30:46 -0400

    checked for nonetype in fin port scan (mode 3)

--- mode3.py
+++ mode3.py
@@ -10,16 +10,30 @@ def mode3():
     # packet =  IP(src="50.106.175.86", dst ="131.229.72.13"/TCP(dport=22, flags="F", seq=12345)
     # p = sr1(packet, timeout=5)
     # p.show()
-    i= IP()
-    t= TCP()
-    i.dst='131.229.72.13'
-    t.dport=22
-    t.flags="F"
-    packet=i/t
-    answered,unanswered = sr(packet, timeout=2)
+    # i= IP()
+    # t= TCP()
+    # i.dst='131.229.72.13'
+    # t.dport=22
+    # t.flags="F"
+    # packet=i/t
+    # print(packet)
+    # answered,unanswered = sr(packet)
+    # print(type(answered))
+    startport = 0
+    endport = 25
+
+    ip1 = IP( dst ="131.229.72.13")
+   
+    
+    sy1 = TCP(dport=20, flags="F", seq=12345)
+    packet = ip1/sy1
+    p =sr1(packet, timeout = 50)   
     #answered.summary(lfilter = lambda s,r: r.sprintf("%TCP.flags%") == "SA",prn=lambda s,r: r.sprintf("%TCP.sport% is open"))
-    if isinstance(answered, type(None)):
-        print('Port' + str(x) + ' is open!')
+    if isinstance(p, type(None)):
+        print('Port' + str(20) + ' is open!')
+    else:
+        print("Port is closed")
+
     # serviceName = socket.getservbyport(1, 'tcp') # this returns just the service name 
     # print(serviceName)
     # print("Name of the service running at port number %d : %s"%(portNumber, serviceName));
