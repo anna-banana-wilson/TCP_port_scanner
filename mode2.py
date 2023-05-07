ee9c18dd5016a6e704ac4394f6e3223c0df91f8d
Jade Yujin Lee <jadeyujinlee@gmail.com>
2023-05-07 19:11:15 -0400

    changingmodes 2 and 3

--- mode2.py
+++ mode2.py
@@ -1,4 +1,59 @@
 from scapy.all import *
 
-def mode2():
-    print("you chose mode 2")
+def mode2(order, ports, target_ip):
+    
+    if(ports == 'all'):
+        endport = 65535
+    elif(ports == 'known'):
+        endport = 1023
+    elif(ports == 'testoption'):
+        endport = 23
+    else:
+        print("Invalid ports argument")
+        exit() # im not sure if this is the right thing, but i want to stop if they type an invalid option
+    
+    # ORDER 
+    if(order == 'order'):
+        for x in range(0, endport):
+            packet = IP(dst = target_ip)/TCP(dport = x, flags = 'S')
+            
+            response = sr1(packet, timeout = 1, verbose = 0) # this sends and recieves one time
+            # print('port: ', x)
+
+            # if actually have a valid response from the target and and we a get a SYN-ACK (0x12 means SYN-ACK)
+            if not isinstance(response, type(None)):
+                if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+                    print(x)
+                    print(socket.getservbyport(x, "tcp"))
+                    # send the RST packet (reset packet, so we can get rid of the connection and move onto the next one):
+                    sr(IP(dst=target_ip)/TCP(dport=response.sport, flags = 'R'), timeout = 0.5, verbose = 0)
+
+    # RANDOM 
+    elif(order == 'random'):
+        port_list = []
+
+        for i in range (0, endport + 1): # the plus 1 is because of how python range() works
+            port_list.append(i)
+            # print('item in port list: ', i) # This number is correct! 
+        
+        random.shuffle(port_list)
+
+        for x in port_list:
+            packet = IP(dst = target_ip)/TCP(dport = x, flags = 'S')
+            response = sr1(packet, timeout = 1, verbose = 0) # this sends and recieves one time
+            
+            # if actually have a valid response from the target and and we a get a SYN-ACK (0x12 means SYN-ACK)
+            if not isinstance(response, type(None)):
+                if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+                    # print(x)
+                    print(socket.getservbyport(x, "tcp"))
+                    # send the RST packet (reset packet, so we can get rid of the connection and move onto the next one):
+                    sr(IP(dst=target_ip)/TCP(dport=response.sport, flags = 'R'), timeout = 0.5, verbose = 0)
+    
+    # INVALID 
+    else: 
+        print("Invalid order argument")
+        exit() # im not sure if this is the right thing, but i want to stop if they type an invalid option
+
+        
+     
--- mode3.py
+++ mode3.py
@@ -1,7 +1,7 @@
 from scapy.all import * 
 import socket
 
-def mode3(): 
+def mode3(ports, mode, target_ip): 
     print("you chose mode 3") 
 
     # ip1 = IP(src="50.106.175.86", dst ="131.229.72.13")
@@ -19,24 +19,63 @@ def mode3():
     # print(packet)
     # answered,unanswered = sr(packet)
     # print(type(answered))
+
+    if(ports == 'all'):
+        endport = 65535
+    elif(ports == 'known'):
+        endport = 1023
+    elif(ports == 'testoption'):
+        endport = 23
+    else:
+        print("Invalid ports argument")
+        exit() # im not sure if this is the right thing, but i want to stop if they type an invalid option
+    
     startport = 0
     endport = 25
 
     ip1 = IP( dst ="131.229.72.13")
 
+    # ORDER 
+    if(order == 'order'):
+        for x in range(startport, endport):
+            sy1 = TCP(dport=x, flags="F", seq=12345)
+            packet = ip1/sy1
+            response = sr1(packet, timeout = 2)   
+            #answered.summary(lfilter = lambda s,r: r.sprintf("%TCP.flags%") == "SA",prn=lambda s,r: r.sprintf("%TCP.sport% is open"))
+            if isinstance(response, type(None)):
+                # print(type(response))
+                serviceName = socket.getservbyport(x, 'tcp') # this returns just the service name 
+                print(serviceName)
 
-    for x in range(startport, endport):
-        sy1 = TCP(dport=x, flags="F", seq=12345)
-        packet = ip1/sy1
-        response = sr1(packet, timeout = 2)   
-        #answered.summary(lfilter = lambda s,r: r.sprintf("%TCP.flags%") == "SA",prn=lambda s,r: r.sprintf("%TCP.sport% is open"))
-        if isinstance(response, type(None)):
-            print(type(response))
-            print("Port " , x , " is open")
+            # serviceName = socket.getservbyport(1, 'tcp') # this returns just the service name 
+            # print(serviceName)
+            # print("Name of the service running at port number %d : %s"%(portNumber, serviceName));
+            # p = sr1(IP(dst="www.slashdot.org")/ICMP()/"XXXXXXXXXXX")
+            # p.show()
 
-        # serviceName = socket.getservbyport(1, 'tcp') # this returns just the service name 
-        # print(serviceName)
-        # print("Name of the service running at port number %d : %s"%(portNumber, serviceName));
-        # p = sr1(IP(dst="www.slashdot.org")/ICMP()/"XXXXXXXXXXX")
-        # p.show()
+    # RANDOM 
+    elif(order == 'random'):
+        port_list = []
+
+        for i in range (0, endport + 1): # the plus 1 is because of how python range() works
+            port_list.append(i)
+            # print('item in port list: ', i) # This number is correct! 
+        
+        random.shuffle(port_list)
+
+        for x in range(startport, endport):
+            sy1 = TCP(dport=x, flags="F", seq=12345)
+            packet = ip1/sy1
+            response = sr1(packet, timeout = 2)   
+            #answered.summary(lfilter = lambda s,r: r.sprintf("%TCP.flags%") == "SA",prn=lambda s,r: r.sprintf("%TCP.sport% is open"))
+            if isinstance(response, type(None)):
+                # print(type(response))
+                serviceName = socket.getservbyport(x, 'tcp') # this returns just the service name 
+                print(serviceName)
+
+    # INVALID 
+    else: 
+        print("Invalid order argument")
+        exit() # im not sure if this is the right thing, but i want to stop if they type an invalid option
+
 mode3()
