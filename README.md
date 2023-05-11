6c9bc0f2c8d27595adf5684a63c7628dc10e458a
anna-banana-wilson <akw02@icloud.com>
2023-05-11 19:49:14 -0400

    added sources, required files, and how to run it

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # TCP_port_scanner
+
+## Sources:
+https://www.paessler.com/it-explained/ping
+https://www.oreilly.com/library/view/python-penetration-testing/9781789138962/9f389f41-4489-4628-a61f-969eea3aae8c.xhtml
+https://pythontic.com/modules/socket/getservbyport
+https://www.youtube.com/watch?v=OKHqhYk198M&ab_channel=CristiVlad
+https://docs.python.org/3/library/argparse.html#type\
+https://docs.python.org/3/library/datetime.html
+https://realpython.com/python-sockets/
+https://www.geeksforgeeks.org/simple-port-scanner-using-sockets-in-python/
+https://gist.github.com/jossef/16d2746565989bd7b7fc02eb794db3b5
+http://pymotw.com/2/socket/tcp.html
+https://0xbharath.github.io/art-of-packet-crafting-with-scapy/network_recon/service_discovery/index.html
+https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
+https://gist.github.com/jossef/16d2746565989bd7b7fc02eb794db3b5 
+
+## Required Files: 
+- port_scanner.py
+- mode1.py
+- mode2.py
+- mode3.py
+
+## How to Run the Port Scanner
+- To run the project, n the terminal, run the following command:
+```Python3 port_scanner.py [mode] [order] [ports] [target_ip]```
+
+- The options for `[mode]` are: 
+    - `normal` which involves a full TCP connect to scan
+    - `syn ` which only sends initial SYN and RST packets to scan
+    - `fin` which only sends FYN packets to scan
+- The options for `[order] ` are:
+    - `order` scans the ports in order starting at 0
+    - `random` scans the ports in a random order
+- The options for `[ports]` are:
+    - `all` scans all 65535 ports
+    - `known` scans only the well known ports, the first 1023
+- The `target_ip` option is simply the IP of the host you wish to scan
+
+The following example runs the project on normal mode, scanning the ports in order, only the first 1023 ports, targeting the host at the ip address 111.111.11.11
+
+```Python3 port_scanner.py normal order known 111.111.11.11```
+
+
---
+++ main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+from pythonping import ping
+import os 
+import argparse 
+import datetime
+import mode1
+import mode2
+import mode3
+import time 
+
+
+def HostUp(hostname, waittime=1000): # this function is from a github user to check ping status 
+    # Function returns True if host IP returns a ping, else False
+    assert isinstance(hostname, str), \
+        "IP/hostname must be provided as a string."
+    if os.system("ping -c 1 -W " + str(waittime) + " " +
+                 hostname + " > /dev/null 2>&1") == 0:
+        HOST_UP = True
+    else:
+        HOST_UP = False
+    return HOST_UP
+    
+
+def main():
+    # positional arguments and options 
+    parser = argparse.ArgumentParser(description="Port Scanner") # initialize ArgumentParser 
+    parser.add_argument("mode", choices=['normal', 'syn', 'fin']) 
+    parser.add_argument("order", choices=['order', 'random'])
+    parser.add_argument("ports", choices=['all', 'known'])
+    parser.add_argument("target_ip", help="list the target host's IP address")
+    args = parser.parse_args() # grab arguments from command line 
+
+    # storing various arguments into variables 
+    mode = args.mode
+    order = args.order 
+    ports = args.ports 
+    target_ip = args.target_ip 
+
+    # using ping to check if target host is up and running 
+    is_alive = HostUp(target_ip, waittime=1000) 
+    
+    if is_alive == True: # if so, then run the port scanner 
+
+        start_time = time.time() 
+        now = datetime.datetime.now()
+        print("Starting port scan at            ", now)
+        print("Interesting ports on ", target_ip, ":")
+        
+        if(mode == 'normal'):
+            num_open = mode1.normal(order = order, ports = ports, target_ip = target_ip)
+        if(mode == 'syn'):
+            num_open = mode2.syn(order = order, ports = ports, target_ip = target_ip)
+        if(mode == 'fin'): 
+            num_open = mode3.fin(order = order, ports = ports, target_ip = target_ip)
+        
+        
+        print("IP address (1 host up) scanned in %s seconds" % (time.time() - start_time))
+
+        if(ports == 'all'):
+            num_closed = 65535 - num_open
+        elif(ports == 'known'):
+            num_closed = 1023 - num_open
+            
+        print("Not shown: %s closed ports" % (num_closed))
+
+    else:
+        print("The target host is down.")
+        exit() 
+    
+
+    
+
+if __name__ == "__main__":
+    
+    main()
+
